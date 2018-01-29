f4c5af8aae7c384e967d83f04f2517291559d893
Bill Horsman <bill@logicalcobwebs.com>
2018-01-29 13:50:10 +0000

    Add links to three main projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,12 @@
 class ProjectsController < ApplicationController
 
   def island_hospital
-    @og[:title] = "Building a Hospital on Gladi Island"
+    @og[:title] = "Building a Hospital on Tasso Island"
     @og[:description] = "The Gladi-Gladi Trust is working with the Island Community to relaunch the Hospital Project."
+  end
+
+  def water_for_children
+    @og[:title] = "Clean Water for the Children of Tasso Island"
+    @og[:description] = "The Gladi-Gladi Trust want to help provide clean water."
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
+
+  get '/projects', to: redirect('/')
+  get '/projects/island-hospital', to: 'projects#island_hospital'
+  get '/projects/water-for-children', to: 'projects#water_for_children'
 end
