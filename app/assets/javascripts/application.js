f1e15d96390f3142375a4f9bbd6d686e4eb67629
Bill Horsman <bill@logicalcobwebs.com>
2018-06-02 09:37:44 +0100

    Switch to Nuxt!

--- app/assets/javascripts/application.js
+++
@@ -1,8 +0,0 @@
-//= require jquery2
-//= require jquery-ui
-//= require popper
-//= require bootstrap-sprockets
-//= require rails-ujs
-//= require turbolinks
-//= require js.cookie
-//= require_tree .
--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  root to: 'welcome#index'
-
-  get '/projects', to: redirect('/')
-  get '/projects/island-hospital', to: 'projects#island_hospital'
-  get '/projects/water-for-children', to: 'projects#water_for_children'
-
-  get '/logo', to: 'welcome#logo' if Rails.env.development?
-end
