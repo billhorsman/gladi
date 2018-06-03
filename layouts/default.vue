a06b6a6f4d670b44557be4c5045b81dd08313f4d
Bill Horsman <bill@logicalcobwebs.com>
2018-06-03 09:49:11 +0100

    Playing with grids

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,11 @@
 <template>
-  <section class="container">
+  <section class="container-fluid">
+    <header>
+      <h1>
+        <img src="~/assets/images/logo.png" alt="Gladi-Gladi" class="logo">
+        Gladi-Gladi Trust
+      </h1>
+    </header>
     <nuxt/>
     <footer>
       &copy; Gladi-Gladi Trust 2017-2018
@@ -8,6 +14,41 @@
 </template>
 
 <style lang="scss">
+  body {
+    background: #713AF3;
+    color: white;
+  }
+  .container-fluid {
+    align-items: center;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 72px 1fr;
+    justify-content: center;
+    min-height: 100vh;
+    text-align: center;
+  }
+  header {
+    grid-column-start: 1;
+    grid-column-end: 4;
+    text-align: left;
+    width: 100%;
+  }
+  h1 {
+    font-size: 2rem;
+    padding: .5rem 0;
+    img {
+      height: 60px;
+      width: 60px;
+    }
+    @media (max-width: 480px) {
+      font-size: 1.5rem;
+    }
+  }
+  section {
+    grid-column-start: 2;
+    grid-column-end: 3;
+    text-align: center;
+  }
   footer {
     bottom: 0;
     position: absolute;
--- pages/index.vue
+++ pages/index.vue
@@ -1,57 +1,37 @@
 <template>
   <section>
-    <div>
-      <h1 class="title">
-        <img src="~/assets/images/logo.png" alt="Gladi-Gladi" class="logo">
-        Gladi-Gladi Trust
-      </h1>
-      <ul class="projects">
-        <li>
-          <a
-            class="logo"
-            href="http://tassoisland.org/">
-            <img src="~/assets/images/tep-logo.png" alt="Gladi-Gladi" class="logo">
-          </a>
-          <h2>Tasso Ecotourism Project</h2>
-          <p>We support ecotourism on Tasso Island in Sierra Leone.</p>
-          <a class="cta"
+    <ul class="projects">
+      <li>
+        <a
+          class="logo"
           href="http://tassoisland.org/">
+          <img src="~/assets/images/tep-logo.png" alt="Gladi-Gladi" class="logo">
+        </a>
+        <h2>Tasso Ecotourism Project</h2>
+        <p>We support ecotourism on Tasso Island in Sierra Leone.</p>
+        <div class="cta">
+          <a class="btn btn-secondary"
+            href="http://tassoisland.org/">
             Find out more
           </a>
-        </li>
-      </ul>
-    </div>
+        </div>
+      </li>
+    </ul>
   </section>
 </template>
 
 <style lang="scss">
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-size: 3rem;
-  img {
-    height: 60px;
-    width: 60px;
-  }
-  @media (max-width: 480px) {
-    font-size: 1.5rem;
-  }
-}
 
 ul.projects {
   margin: 2rem 0 0;
   padding: 0;
 
   > li {
+    background: rgba(255, 255, 255, .6);
+    color: black;
     list-style: none;
     margin: 0;
-    padding: 0;
+    padding: 1rem;
     text-align: left;
   }
 
@@ -68,6 +48,10 @@ ul.projects {
     font-size: 1.3rem;
   }
 
+  .cta {
+    text-align: right;
+  }
+
   @media (max-width: 480px) {
     > li {
       text-align: center;
@@ -79,14 +63,4 @@ ul.projects {
   }
 }
 
-.links {
-  margin-top: 2rem;
-  a {
-    padding: 1rem;
-    img {
-      height: 100px;
-      width: 100px;
-    }
-  }
-}
 </style>
